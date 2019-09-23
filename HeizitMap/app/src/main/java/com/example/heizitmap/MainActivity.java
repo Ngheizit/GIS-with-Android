cac6b62e473876e2f131d29c9828e37f82387159
Ngheizit <ngheizitwu@163.com>
2019-09-23 19:34:10 +0800

    Create HeizitMap

---
+++ HeizitMap/app/src/main/java/com/example/heizitmap/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.heizitmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // *** ADD ***
+        mMapView = findViewById(R.id.axMapView);
+        setupMap();
+    }
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 34.09042;
+            double longitude = -118.71511;
+            int levelOfDetail = 11;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+
+}
