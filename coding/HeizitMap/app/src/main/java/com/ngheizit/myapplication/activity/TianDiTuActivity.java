c0689fa443c6b322b85a8bcd93565e96c258aed9
Ngheizit <ngheizitwu@163.com>
2019-10-05 10:16:43 +0800

    20191005 tdt poi

--- coding/HeizitMap/app/src/main/java/com/ngheizit/myapplication/activity/TianDiTuActivity.java
+++ coding/HeizitMap/app/src/main/java/com/ngheizit/myapplication/activity/TianDiTuActivity.java
@@ -2,6 +2,9 @@ package com.ngheizit.myapplication.activity;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,8 +24,20 @@ import com.ngheizit.myapplication.TianDiTuMethodsClass;
 import com.ngheizit.myapplication.ToastUtil;
 import com.ngheizit.myapplication.gps.GPSLocationListener;
 import com.ngheizit.myapplication.gps.GPSLocationManager;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.Call;
 
 
 public class TianDiTuActivity extends AppCompatActivity {
@@ -44,13 +59,36 @@ public class TianDiTuActivity extends AppCompatActivity {
     private WebTiledLayer webTiledLayer2;
 
 
+    @BindView(R.id.btn_locate) Button btn_locate;
+    @BindView(R.id.btn_zoomIn) Button btn_zoomIn;
+    @BindView(R.id.btn_zoomOut) Button btn_zoomOut;
+    @OnClick({
+            R.id.btn_locate, R.id.btn_zoomIn, R.id.btn_zoomOut
+    })public void onViewClicked(View view){
+        switch (view.getId()){
+            case R.id.btn_zoomIn:
+                pMapView.setViewpointScaleAsync(pMapView.getMapScale()*0.5 );
+                break;
+            case R.id.btn_zoomOut:
+                pMapView.setViewpointScaleAsync(pMapView.getMapScale()*2 );
+                break;
+            case R.id.btn_locate:
+                Point point =new Point( lon,lat) ;
+                search_poi("博物馆");
+                pMapView.setViewpointCenterAsync( point,20329.6);
+                break;
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tian_di_tu);
+        ButterKnife.bind(this);
+
 
         pMapView = findViewById(R.id.axMapView);
-
         gpsLocationManager = GPSLocationManager.getInstances(TianDiTuActivity.this);
         gpsLocationManager.start(new MyListener());
 
@@ -82,6 +120,48 @@ public class TianDiTuActivity extends AppCompatActivity {
             mGraphicsOverlay = new GraphicsOverlay();
             pMapView.getGraphicsOverlays().add(mGraphicsOverlay);
         }
+    }
+
+    private void search_poi(String keword){
+        JSONObject jo = new JSONObject();
+        try {
+            jo.put("keyWord", keword);
+            jo.put("level", "11");
+            jo.put("mapBound", "113.51576352656,37.437345659733,115.47631654589,38.763868632776");
+            jo.put("queryType", "1");
+            jo.put("count", "20");
+            jo.put("start", "0");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        OkHttpUtils.get()
+                .url("http://api.tianditu.gov.cn/search")
+                .addParams("postStr", jo.toString())
+                .addParams("type", "query")
+                .addParams("tk", "92d4f72edb2b6dcef6e8a6a60cf88d4f")
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        e.printStackTrace();
+                    }
+                    @Override
+                    public void onResponse(String response, int id) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray pois = jsonObject.getJSONArray("pois");
+                            for (int i = 0; i < pois.length(); i++){
+                                JSONObject jo = pois.getJSONObject(i);
+                                String address = jo.getString("address");
+                                String name = jo.getString("name");
+                                String lonlat = jo.getString("lonlat");
+                                System.out.println("&&&&&&&&&&&&  " + address + " " + name + " " + lonlat);
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
     }
 
     @Override
@@ -123,7 +203,7 @@ public class TianDiTuActivity extends AppCompatActivity {
                 lon = location.getLongitude();
                 bearing = location.getBearing();
 
-                System.out.println(lat + "XXX" + lon + "XXX" + bearing);
+                // System.out.println(lat + "XXX" + lon + "XXX" + bearing);
                 //38.04351294         114.60884694
                 //TransformationCatalog.getTransformationsBySuitability()
             }
